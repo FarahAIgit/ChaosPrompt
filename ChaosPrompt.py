881743a940a61077bd5e30e0531feb92a7d27d4e
FarahAIgit <sanguinerose90@gmail.com>
2025-11-20 12:52:37 +0000

    Update ChaosPrompt.py

--- ChaosPrompt.py
+++ ChaosPrompt.py
@@ -74,8 +74,11 @@ if st.button("Generate Prompt"):
 st.markdown(
     "Created by [@Farah_ai_](https://x.com/Farah_ai_)",
     unsafe_allow_html=True
-    "Use of this generator is free but if you find it useful please consider donating a little; [Donate via Kofi](https://ko-fi.com/farahai)",
+)
+st.markdown(
+     "Use of this generator is free but if you find it useful please consider donating a little; [Donate via Kofi](https://ko-fi.com/farahai)",
     unsafe_allow_html=True
 )
+
 
 
